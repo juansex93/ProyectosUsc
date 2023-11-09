95e37dcf1822166f023aa4dbe47f5dceeb4ad90b
Gonorreator <harolddanielzuniga64@gmail.com>
2023-11-09 13:20:42 -0500

    Clean Logs Of Git

--- carwash/login/views/login.php
+++ carwash/login/views/login.php
@@ -102,7 +102,7 @@
                 <!-- You can delete the links only if you purchased the pro version. -->
                 <!-- Licensing information: https://bootstrapmade.com/license/ -->
                 <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-                Dieseño por <a href="https://sebastiantates.blogspot.com/">Juansex</a>
+                Dieseño por <a href="https://sebastiantates.blogspot.com/">Nameless</a>
               </div>
 
             </div>
